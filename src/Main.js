3d4db3f8eac66fbfd9aa046412984189d8eb6758
Michael Bock <me@thought2.de>
2018-09-07 11:54:27 +0200

    enhance js

--- src/Main.js
+++ src/Main.js
@@ -12,3 +12,17 @@ exports.readWFObj = function(objString) {
     console.log(output)
     return output;
 }
+
+function main() {
+    var canvas = document.getElementById("glcanvas");
+
+    var resize = function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    resize();
+    window.onresize = resize;
+}
+
+main();
